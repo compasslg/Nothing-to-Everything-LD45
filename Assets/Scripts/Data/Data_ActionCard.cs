45a3769c4e29cfaa6470c82e954930d0337ec840
compasslg <lihanwei4c@gmail.com>
2019-10-04 18:26:18 -0700

    Data_ActionCard

---
+++ Assets/Scripts/Data/Data_ActionCard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data_ActionCard {
+	public readonly string cardName;
+	public readonly Sprite icon;
+	public readonly string description;
+	public readonly int manaCost;
+	public readonly int enemyHPLoss, enemyMPLoss, selfHPRegen, selfMPRegen;
+	public readonly bool evasion;
+	public readonly int dmgBlock;
+	public Data_ActionCard(string cardName, Sprite icon, int manaCost, int enemyHPLoss, 
+	int enemyMPLoss, int selfHPRegen, int selfMPRegen, int evasion, int dmgBlock){
+		this.cardName = cardName;
+		this.icon = icon;
+		this.manaCost = manaCost;
+		this.enemyHPLoss = enemyHPLoss;
+		this.enemyMPLoss = enemyMPLoss;
+		this.selfHPRegen = selfHPRegen;
+		this.selfMPRegen = selfMPRegen;
+		this.dmgBlock = dmgBlock;
+		if(evasion == 0){
+			this.evasion = false;
+		}else{
+			this.evasion = true;
+		}
+	}
+}
--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using System.IO;
 public class DeckManager : MonoBehaviour {
-
+	private int totalChance;
+	private Dictionary<int, Data_ActionCard> actionCards;
 	// Use this for initialization
 	void Start () {
 		
@@ -18,8 +20,23 @@ public class DeckManager : MonoBehaviour {
         StreamReader reader = new StreamReader("Assets/Data/actioncards.data");
         string[] line = null;
         string[] firstLine = reader.ReadLine().Split('\t');
+		totalChance = 0;
+		int curChance = 0;
+		int manaCost = 0;
+		int enemyHPLoss = 0;
+		int enemyMPLoss = 0;
+		int selfHPRegen = 0;
+		int selfMPRegen = 0;
         while(!reader.EndOfStream){
             line = reader.ReadLine().Split('\t');
+			Sprite icon = Resources.Load<Sprite>("Sprites/" + line[0] + ".png");
+			totalChance += Convert.ToInt32(line[3]);
+			manaCost = Convert.ToInt32(line[2]);
+			enemyHPLoss = Convert.ToInt32(line[4]);
+			enemyMPLoss = Convert.ToInt32(line[5]);
+			selfHPRegen = Convert.ToInt32(line[6]);
+			selfMPRegen = Convert.ToInt32(line[7]);
+			Data_ActionCard card = new Data_ActionCard(line[0],icon,manaCost,enemyHPLoss,enemyMPLoss,selfHPRegen,selfMPRegen);
 		}
 	}
 }
